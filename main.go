e175de846bde49b60b59831eb9fb8a2b6a50a7c6
Jacob George <kooldrjay060@gmail.com>
2025-09-22 13:13:31 +0100

    added webserver listen and serve

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type Person struct {
@@ -21,6 +22,7 @@ func main() {
 
 	xp := []Person{p1, p2}
 
+	// format to json
 	bs, err := json.Marshal(xp)
 
 	if err != nil {
@@ -28,4 +30,26 @@ func main() {
 	}
 
 	fmt.Println(string(bs))
+
+	xp2 := []Person{}
+	err = json.Unmarshal(bs, &xp2)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Return to go structure", xp2)
+
+	// serving a web application with go
+	http.HandleFunc("/encoder", foo)
+	http.HandleFunc("/decoder", bar)
+	http.ListenAndServe(":80", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }
