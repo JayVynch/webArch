62dea3db1fc200ea0affdc493fc79448322e2f13
Jacob George <kooldrjay060@gmail.com>
2025-09-22 13:45:33 +0100

    added an example decoder

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,44 +11,59 @@ type Person struct {
 }
 
 func main() {
-	p1 := Person{
-		First: "James",
-	}
+	// p1 := Person{
+	// 	First: "James",
+	// }
 
-	p2 := Person{
-		First: "Daniel",
-	}
+	// p2 := Person{
+	// 	First: "Daniel",
+	// }
 
-	xp := []Person{p1, p2}
+	// xp := []Person{p1, p2}
 
-	// format to json
-	bs, err := json.Marshal(xp)
+	// // format to json
+	// bs, err := json.Marshal(xp)
 
-	if err != nil {
-		log.Panic(err)
-	}
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Println(string(bs))
+	// fmt.Println(string(bs))
 
-	xp2 := []Person{}
-	err = json.Unmarshal(bs, &xp2)
+	// xp2 := []Person{}
+	// err = json.Unmarshal(bs, &xp2)
 
-	if err != nil {
-		log.Panic(err)
-	}
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Println("Return to go structure", xp2)
+	// fmt.Println("Return to go structure", xp2)
 
 	// serving a web application with go
 	http.HandleFunc("/encoder", foo)
 	http.HandleFunc("/decoder", bar)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := Person{
+		First: "James",
+	}
 
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Bad json", err)
+	}
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
+	var p1 Person
 
+	err := json.NewDecoder(r.Body).Decode(&p1)
+
+	if err != nil {
+		log.Println("Bad json", err)
+	}
+
+	log.Println("decoded Person:", p1)
 }
